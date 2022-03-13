717f0458c08787f1b6bb13f46f1c4f5b193f9c11
Simeon_YT <61916863+Simeon53424@users.noreply.github.com>
2022-03-13 21:26:25 +0100

    Add files via upload

---
+++ bsod.bat
@@ -0,0 +1,22 @@
+@echo off
+title BSOD trigger.exe
+color A
+cls
+pause
+title BSOD trigger.exe > Do Do you want to continue?
+cls
+pause
+title BSOD trigger.exe > do Are you sure?
+cls
+pause
+title BSOD trigger.exe > do are you really sure?
+cls
+pause
+title BSOD trigger.exe > do are you really really sure?
+cls
+pause
+cls
+title BSOD trigger.exe > do Don't say I didn't warn you
+cls
+pause
+powershell Start-Process bsodgo.bat -Verb runAs
